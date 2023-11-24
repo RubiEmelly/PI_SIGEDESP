96341ae61b370177cb4dbba1d9c751c9b736146f
jeffersonarpasserini <jefferson.passerini@gmail.com>
2023-11-24 13:45:39 -0300

    ajuste bootstrp em fomrularios de tipo intituicao

--- wwwroot/js/demo/datatables-demo.js
+++ wwwroot/js/demo/datatables-demo.js
@@ -1,4 +1,23 @@
 // Call the dataTables jQuery plugin
-$(document).ready(function() {
-  $('#dataTable').DataTable();
+
+$(document).ready(function () {
+    $('#dataTable').DataTable({
+        "oLanguage": {
+            "sProcessing": "Processando...",
+            "sLengthMenu": "Mostrar _MENU_ registros",
+            "sZeroRecords": "Nenhum registro encontrado.",
+            "sInfo": "Mostrando de _START_ até _END_ de _TOTAL_ registros",
+            "sInfoEmpty": "Mostrando de 0 ate 0 de 0 registros",
+            "sInfoFiltered": "",
+            "sInfoPostFix": "",
+            "sSearch": "Buscar:",
+            "sUrl": "",
+            "oPaginate": {
+                "sFirst": "Primeiro",
+                "sPrevious": "Anterior",
+                "sNext": "Seguinte",
+                "sLast": "Último"
+            }
+        }
+    });
 });
