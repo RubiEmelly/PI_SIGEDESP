065e660db5c30ceed327eea62c8dde6ebbd26a05
RubiEmelly <emellyandrade.rubiyasmin@gmail.com>
2023-12-01 00:47:30 -0300

    Alterações Feitas na Documentação. UnidadeConsumidora criada, mas precisa testar pra ver se funciona

---
+++ Controllers/UnidadeConsumidoraController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SIGEDESP_PI.Controllers
+{
+    public class UnidadeConsumidoraController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/UnidadeConsumidora.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SIGEDESP_PI.Models;
+
+[Table("unidadeconsumidora")]
+public class UnidadeConsumidoraModel
+{
+    [Key]
+    [Column("id")]
+    public int Id { get; set; }
+
+    [Column("codigouc")]
+    [Required(ErrorMessage = "O código da unidade consumidora deve ser informada")]
+    [Display(Name = "Código Unidade Consumidora")]
+    [MinLength(1, ErrorMessage = "Descrição deve ter no mínimo {1} caracteres")]
+    [MaxLength(100, ErrorMessage = "Descrição pode exceder {1} caracteres")]
+    public int CodigoUC { get; set; }
+}
